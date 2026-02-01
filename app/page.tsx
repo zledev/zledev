ca702526ed74c5ee9ccebce54a82eba61538d86a
zleDev <182633917+zledev@users.noreply.github.com>
2026-02-01 14:39:45 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/sections/Hero";
-import Projects from "@/components/sections/Projects";
 import FooterLast from "@/components/sections/Footer";
 import ProjectView from "@/components/sections/ProjectView";
 
